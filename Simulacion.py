4bed50f3a7406106d4538b6be3b5dd2ee263c74d
yufyuf02 <che242115@uvg.edu.gt>
2026-02-26 12:53:20 -0600

    Inicio de Simulacion

---
+++ Simulacion.py
@@ -0,0 +1,13 @@
+"""
+HDT5: Simulación de corrida de programas
+Yu-Fong Chen (242115) y Milena Rodriguez ()
+"""
+
+import sympy
+import random
+import statistics
+import matplotlib.pyplot as plt         # Para hacer las gráficas
+
+
+
+# FIN DE PROGRAMA
